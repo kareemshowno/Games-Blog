79383c6d043c477c107f9af58b7b20feee458d20
kareemshowno <65797037+kareemshowno@users.noreply.github.com>
2020-10-20 23:48:37 +0300

    Updates

---
+++ precache-manifest.1a5b93c7bc8242e6337c9f79e991c5b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f813f27f83373290cd8ae6516a424e57",
+    "url": "/Games-Blog/index.html"
+  },
+  {
+    "revision": "e712349a07aa7fb55bbb",
+    "url": "/Games-Blog/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3a0bfb1b495111ab2de8",
+    "url": "/Games-Blog/static/css/main.3d2e0dc4.chunk.css"
+  },
+  {
+    "revision": "e712349a07aa7fb55bbb",
+    "url": "/Games-Blog/static/js/2.b168058e.chunk.js"
+  },
+  {
+    "revision": "98a8390e84b8683475e2d7a7cad3be88",
+    "url": "/Games-Blog/static/js/2.b168058e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0bfb1b495111ab2de8",
+    "url": "/Games-Blog/static/js/main.55352f13.chunk.js"
+  },
+  {
+    "revision": "4fb5cf152f460d79021e",
+    "url": "/Games-Blog/static/js/runtime-main.cfc8706e.js"
+  },
+  {
+    "revision": "95e8ef63507c1b354b10f1a08cd01624",
+    "url": "/Games-Blog/static/media/Forza-horizon_1920x1080.95e8ef63.png"
+  },
+  {
+    "revision": "c849a3de25f56db6c0399280acded869",
+    "url": "/Games-Blog/static/media/assassins.c849a3de.jpg"
+  },
+  {
+    "revision": "f97a8deb6b8e1f732a0e5b2224177a48",
+    "url": "/Games-Blog/static/media/battelfield_1_1920x1080.f97a8deb.jpg"
+  },
+  {
+    "revision": "616f689b66889d1cac8c6c8a47a1fc87",
+    "url": "/Games-Blog/static/media/hitman.616f689b.jpg"
+  }
+]);
